bdba6ffb896c6a224491fbddab66a6d74efd9105
Emin Ar <82047546+emin350@users.noreply.github.com>
2024-01-23 15:24:10 +0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,77 @@
+import HeadingText from '@/components/HeadingText'
+import Navbar from '@/components/Navbar'
+import { deleteContact } from '@/lib/action'
+import { getContacts } from '@/lib/data'
+import Link from 'next/link'
+//import React from 'react'
+import {FiTrash, FiEdit} from "react-icons/fi"
+
+import React from 'react'
+
+const HomePage = async() => {
+  const contacts = await getContacts()
+  return (
+    <main>
+      <HeadingText
+      title='Contact Book'
+      description='All Contacts Below'
+      />
+      <Navbar/>
+      <div className='flex flex-col px-4 py-4'>
+        <div>
+          <table className='table-auto w-full text-center whitespace-nowrap'>
+             <thead className='text-sm bg-gray-100'>
+              <tr>
+                <th className='px-4 py-3 title-font text-center tracking-wider font-medium text-gray-700'>
+                  First Name
+                </th>
+                <th className='px-4 py-3 title-font text-center tracking-wider font-medium text-gray-700'>
+                  Last Name
+                </th>
+                <th className='px-4 py-3 title-font text-center tracking-wider font-medium text-gray-700'>
+                  Email
+                </th>
+                <th className='px-4 py-3 title-font text-center tracking-wider font-medium text-gray-700'>
+                  Contact
+                </th>
+                <th className='px-4 py-3 title-font text-center tracking-wider font-medium text-gray-700'>
+                  Action
+                </th>
+              </tr>
+             </thead>
+             <tbody>
+            {
+              contacts.map((contact) => (
+                <tr key={contact.id}>
+                  <td className='border-t-2 text-center border-gray-200 px-4 py-3'>{contact.firstName}</td>
+                  <td className='border-t-2 text-center border-gray-200 px-4 py-3'>{contact.lastName}</td>
+                  <td className='border-t-2 text-center border-gray-200 px-4 py-3'>{contact.email}</td>
+                  <td className='border-t-2 text-center border-gray-200 px-4 py-3'>{contact.phone}</td>
+                  <td className='border-t-2 text-center border-gray-200 px-4 py-3 flex items-center gap-1'>
+                  <Link href={`contact/${contact.id}`}>
+                    <FiEdit />
+                  </Link>
+                  <form
+                  action={deleteContact}
+                  >
+                    <input hidden name='id' value={contact.id} />
+                  <button
+                  type='submit'
+                  className='mt-2'
+                  >
+                  <FiTrash style={{color: 'red'}} />
+                  </button>
+                  </form>
+                  </td>
+                </tr>
+              ))
+            }
+          </tbody>
+          </table>
+        </div>
+        </div>
+    </main>
+  )
+}
+
+export default HomePage
